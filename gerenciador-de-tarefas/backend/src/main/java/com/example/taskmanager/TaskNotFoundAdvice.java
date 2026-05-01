1bb3ba89ab101e8dd204b714a0444de601a44cc0
Feliphe Blatt <felipheblatt@gmail.com>
2026-04-30 22:12:23 -0300

    feat: add task crud backend base

---
+++ gerenciador-de-tarefas/backend/src/main/java/com/example/taskmanager/TaskNotFoundAdvice.java
@@ -0,0 +1,17 @@
+package com.example.taskmanager;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class TaskNotFoundAdvice {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handle(TaskNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", exception.getMessage()));
+    }
+}
